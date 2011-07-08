3b128fa7b03c7a44f06e9e0d1f4ccc62037fdd24
Trygve Laugstøl <trygvis@inamo.no>
2011-07-08 17:42:10 +0200

    o Adding more docs with examples.

--- unix-handbook/src/main/docbook/examples/run-basic.sh
+++ unix-handbook/src/main/docbook/examples/run-basic.sh
@@ -9,10 +9,16 @@ then
   MVN=mvn
 fi
 
+SED1="s,\(.*Installing\) /[a-zA-Z/\.-]*\(/unix/.*\),\1 ~\2,g"
+SED2="s,\(.*to\) /[a-z]*/[a-z]*\(.*\),\1 ~\2,g"
+SED3="s,\(.*resourceDirectory\) /[a-zA-Z/\.-]*\(/unix/.*\),\1 ~\2,g"
+
 if [ -x "`which pkgchk 2>/dev/null`" ]
 then
   rm -rf target
-  "${MVN}" -f pom-pkg.xml clean install > mvn-pkg.txt 2>&1
+  "${MVN}" -Dencoding=UTF-8 -f pom-pkg.xml clean install |
+    sed -e "${SED1}" -e "${SED2}" -e "${SED3}" \
+    > mvn-pkg.txt 2>&1
 
   cmd="pkgchk -l -d target/basic-*.pkg all"
   echo "$ $cmd" > pkgchk.txt
@@ -28,7 +34,9 @@ fi
 if [ -x "`which rpmbuild 2>/dev/null`" ]
 then
   rm -rf target
-  "${MVN}" -f pom-rpm.xml clean install > mvn-rpm.txt 2>&1
+  "${MVN}" -Dencoding=UTF-8 -f pom-rpm.xml clean install |
+    sed -e "${SED1}" -e "${SED2}" -e "${SED3}" \
+    > mvn-rpm.txt 2>&1
 
   cmd="rpm -q -v -l -p target/basic-*.rpm"
   echo "$ $cmd" > rpm-qvlp.txt
@@ -39,8 +47,26 @@ then
   $cmd >> rpm-qlp.txt
 fi
 
+if [ -x "`which dpkg-deb 2>/dev/null`" ]
+then
+  rm -rf target
+  "${MVN}" -Dencoding=UTF-8 -f pom-deb.xml clean install |
+    sed -e "${SED1}" -e "${SED2}" -e "${SED3}" \
+    > mvn-deb.txt 2>&1
+
+  cmd="dpkg-deb -c target/*.deb"
+  echo "$ $cmd" > dpkg-deb-c.txt
+  $cmd >> dpkg-deb-c.txt
+
+  cmd="dpkg-deb -f target/*.deb"
+  echo "$ $cmd" > dpkg-deb-f.txt
+  $cmd >> dpkg-deb-f.txt
+fi
+exit
 rm -rf target
-"${MVN}" -f pom-zip.xml clean install > mvn-zip.txt 2>&1
+"${MVN}" -Dencoding=UTF-8 -f pom-zip.xml clean install |
+    sed -e "${SED1}" -e "${SED2}" -e "${SED3}" \
+    > mvn-zip.txt 2>&1
 
 cmd="unzip -l target/basic-*.zip"
 echo "$ $cmd" > unzip-l.txt
